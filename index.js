90fda675a268cea37cf56af4e9d17b1575ccacd6
Mohammad815 <mohammadforhad176@gmail.com>
2021-09-02 16:22:24 +0600

    design ui

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ document.getElementById('search-button').addEventListener('click',function(){
     //console.log(searchText)
 })
 const displaySearchResult = data =>{
-  if(data.length == 0){
+  if(data.length === 0){
     errorDiv.innerText = 'Please Write your correct book name'
   }
   else{
@@ -29,17 +29,13 @@ const displaySearchResult = data =>{
     totalLength.innerText = `Total Datal : ${data.length}`
     //console.log(data.length);
     data.forEach(data =>{
-        //console.log(data);
-        /* 
-        <img src="..." class="card-img-top" alt="...">
-        */
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML=`
         <div class="card h-100">
             <div class="card-body">
             <img src="https://covers.openlibrary.org/b/id/${data.cover_i}-M.jpg" class="card-img-top" alt="...">
-            <h1 class="card-title">Subject Name :  ${data.title}</h1>
+            <h1 class="card-title">Book Name :  ${data.title}</h1>
               <h5 class="card-title">Author Name: ${data.author_name}</h5>
               <h5 class="card-title">First Published Date :  ${data.first_publish_year}</h5>
               <p class="card-text">This is a longer.</p>
