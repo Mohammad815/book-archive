4d555219b0df90ca9cdd94b979cae3a407255432
Mohammad815 <mohammadforhad176@gmail.com>
2021-09-02 19:31:47 +0600

    setting tag

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 const errorDiv = document.getElementById('error');
 const totalLength = document.getElementById('total-length');
 document.getElementById('search-button').addEventListener('click',function(){
-    const searchField = document.getElementById('search-field');
-    const searchText = searchField.value;
-    searchField.value ='';
+  const searchField = document.getElementById('search-field');
+  const searchText = searchField.value;
+  searchField.value ='';
 
   if(searchText === ''){
     errorDiv.innerText ='Please Write Somthing'
   }
   else{
-    const url = `https://openlibrary.org/search.json?q=${searchText}`;
-    fetch(url)
+    //  Loaa Data 
+     const url = `https://openlibrary.org/search.json?q=${searchText}`;
+     fetch(url)
     .then(res => res.json())
     .then(data => displaySearchResult(data.docs))
   }
 
-    //console.log(searchText)
+    // Show the Result
 })
 const displaySearchResult = data =>{
   if(data.length === 0){
@@ -24,28 +25,24 @@ const displaySearchResult = data =>{
   else{
     errorDiv.innerText='';
   }
-    //console.log(data)
-    const row = document.getElementById('row');
-    totalLength.innerText = `Total Book Found : ${data.length}`
-    row.textContent = '';
-    //console.log(data.length);
-    data.forEach(data =>{
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML=`
-        <div class="card h-100">
-            <div class="card-body">
-            <img src="https://covers.openlibrary.org/b/id/${data.cover_i}-M.jpg" class="card-img-top" alt="...">
-            <h1 class="card-title">Book Name :  ${data.title}</h1>
-              <h5>Author Name: ${data.author_name}</h5>
-              <h5>First Published Date :  ${data.first_publish_year}</h5>
-              <h5 class="card-title">Publisher :  ${data.publisher}</h5>
-              
-            </div>
-          </div>
+  
+  const row = document.getElementById('row');
+  totalLength.innerText = `Total Book Found : ${data.length}`
+  row.textContent = '';
+  data.forEach(data =>{
+    const div = document.createElement('div');
+    div.classList.add('col');
+    div.innerHTML=`
+      <div class="card h-100">
+        <div class="card-body">
+          <img src="https://covers.openlibrary.org/b/id/${data.cover_i}-M.jpg" class="card-img-top" alt="...">
+          <h1 class="card-title">Book Name :  ${data.title}</h1>
+          <h5>Author Name: ${data.author_name}</h5>
+          <h5>First Published Date :  ${data.first_publish_year}</h5>
+          <h5 class="card-title">Publisher :  ${data.publisher}</h5>
+        </div>
+      </div>
         `
-        row.appendChild(div);
-
-   
+    row.appendChild(div);
     });
 }
